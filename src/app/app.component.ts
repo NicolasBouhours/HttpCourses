162a7473647fd7d0d93e76b949bb985440825ed8
NicolasBouhours <nico53960@hotmail.fr>
2016-11-26 10:21:15 +0100

    STEP 159 - Sending a GET Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/Http';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  
+  constructor(private httpService: HttpService) { }
+
+  ngOnInit() {
+    this.httpService.getData().subscribe(
+      (data: Response) => console.log(data)
+    );
+  }
 }
